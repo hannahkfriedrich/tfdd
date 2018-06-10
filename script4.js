e11c78f3c70e860ba5cf47bfb8b0a131b9df1bd3
Giovanni Svevo <38077878+gsvevo@users.noreply.github.com>
2018-06-09 21:18:18 -0700

    Add files via upload

--- script4.js
+++ script4.js
@@ -34,12 +34,12 @@ var tabulate = function (data,columns) {
  
 var tTable = function(){
     d3.csv("assets/treaties.csv").then(function (data) {
-     
+ 
         var columns = ['Basin','Country','Description', 'DateSigned','IssueArea'];
-     
+ 
         tabulate(data,columns)
     })
-    };
+};
 /////////////////////////////////////////THE CALLS////////////////////////////////////////////////////////////////////
  
 tTable();
@@ -63,7 +63,7 @@ window.onload = function() {
     console.log("window loaded")
     var a = document.getElementById('backbutton');
  
-    }
+}
  
  
 //////////////////////////////////////////ATLAS////////////////////////////////////////////////
@@ -85,7 +85,7 @@ function resize(){
  
  
 /*window.onresize = function() {
-     
+ 
     var width = $(window).width();
     var height = $(window).height();
     $("svg").attr("viewBox", "(0 , 0, " + height + ", " + width  + ")");
@@ -97,11 +97,9 @@ function resize(){
  
 var color = d3.scaleOrdinal(d3.schemePaired);
 //var color = d3.scaleOrdinal(['#4F6168','#6E7E85','#2A9D8F','#7C9EB2','#2A9D8F','#476A6F']);
-var projection = d3.geoMercator()
-    .scale([width * 0.1])
+var projection = d3.geoBromley()
+    .scale([width * 0.15])
     .translate([width / 2, height / 2]);
- 
- 
  
  
 var path = d3.geoPath().projection(projection);
@@ -128,7 +126,7 @@ files.forEach(function(url) {
     promises.push(d3.json(url))
 });
  
-Promise.all(promises).then(function(results) {  
+Promise.all(promises).then(function(results) {
  
     var graph = results[0];
     var features = results[1].features;
@@ -143,9 +141,9 @@ Promise.all(promises).then(function(results) {
         .attr('width', width)
         .attr('height', height);
  
-//add encompassing group for the zoom 
-var g = svg.append("g")
-    .attr("class", "everything");
+//add encompassing group for the zoom
+    var g = svg.append("g")
+        .attr("class", "everything");
  
     var map = g.append('g')
         .attr('class', 'map')
@@ -153,7 +151,7 @@ var g = svg.append("g")
         .data(features)
         .enter().append('path')
         .attr('d', path)
-         
+ 
     var links = g.append('g')
         .attr('class', 'links')
         .selectAll('line')
@@ -161,39 +159,39 @@ var g = svg.append("g")
         .enter().append('line')
         .attr('stroke-width', function (d) { return d.count/6 })
  
-     var nodes = g.append("g")
-        .attr("class", "nodes") 
+    var nodes = g.append("g")
+        .attr("class", "nodes")
         .selectAll("g")
         .data(graph.nodes)
         .enter()
         .append("g")
-        .call(drag)        
+        .call(drag)
         .on('click', connectedNodes)
-         
+ 
     var circles = nodes.append("circle")
-        .attr("r", function(d) {  if (positioning === 'map') {return d.value/6;}} )
+        .attr("r", function(d) {  if (positioning === 'map') {return d.value/5;}} )
         .attr("fill", function(d) { return color(d.group); })
         .on('click', searchCountry)
-         //Added code ;
+    //Added code ;
  
-nodes.append("text")
+    nodes.append("text")
         .text(function(d) {if (d.group === 'Basin'){
-        return d.id;} else {}
+            return d.id;} else {}
         }).style("font-size", 2.5)
-            .style("font-family", 'sans-serif')
-                .attr('x', 1)
-                .attr('y', 1)
+        .style("font-family", 'sans-serif')
+        .attr('x', 1)
+        .attr('y', 1)
  
-/*        .text(function(d) {
-        return d.id;
-        }).style("font-size", 2)
-            .style("font-family", 'sans-serif')
-                .attr('x', 1)
-                .attr('y', 1)
-        .style("fill", function(d) {if (d.group != 'Basin'){
-            return "red";} else {return "green"}
-        })
-  */
+    /*        .text(function(d) {
+            return d.id;
+            }).style("font-size", 2)
+                .style("font-family", 'sans-serif')
+                    .attr('x', 1)
+                    .attr('y', 1)
+            .style("fill", function(d) {if (d.group != 'Basin'){
+                return "red";} else {return "green"}
+            })
+      */
     nodes.append('title')
         .text(function (d) { return d.id + ': ' + d.value + ' relations'})
     links.append('title')
@@ -230,8 +228,8 @@ nodes.append("text")
             .attr('cx', function (d) { return d.x })
             .attr('cy', function (d) { return d.y })
             .attr("transform", function(d) {
-          return "translate(" + d.x + "," + d.y + ")";
-        })
+                return "translate(" + d.x + "," + d.y + ")";
+            })
     }
  
     function toggle() {
@@ -243,22 +241,22 @@ nodes.append("text")
             circles.attr("r", function(d) { return d.value/2; })
             nodes.append("text")
                 .text(function(d) {
-                return d.id;
+                    return d.id;
                 }).style("font-size", 12).style("font-family", 'sans-serif')
                 .attr('x', 12)
                 .attr('y', 3)
-            circles.style("stroke-width", 0.5 ) 
+            circles.style("stroke-width", 0.5 )
             zoom_actions()
-        } 
+        }
     }
  
     function searchCountry(e){
-       // var layer = e.target;
+        // var layer = e.target;
         var value = e.id;
         $("#myTable tr").filter(function () {
             $(this).toggle($(this).text().indexOf(value) > -1)
         });
-         
+ 
     }
 //FUNCTION TO HIGHLIGHT NODES
  
@@ -278,7 +276,7 @@ nodes.append("text")
     }
  
  
-     
+ 
     function connectedNodes() {
         if (toggle == 0) {
             //Reduce the opacity of all but the neighbouring nodes
@@ -297,131 +295,131 @@ nodes.append("text")
             links.style("opacity", 1);
             toggle = 0;
         }
-    } 
-        
- 
-//add zoom capabilities 
-var zoom_handler = d3.zoom()
-    .on("zoom", zoom_actions);
- 
-zoom_handler(svg);     
-//Zoom functions 
-function zoom_actions(){
-    //if (positioning === 'map') { return }
-    g.attr("transform", d3.event.transform)
-}
- 
-function tickActions() {    
-    links
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
- 
-    nodes
-        .attr("transform", function(d) {
-          return "translate(" + d.x + "," + d.y + ")";
-        })
-}
- 
-/* LEGEND
-var legend = svg.selectAll(".legend")
-    .data(color.domain())
-    .enter().append("g")
-    .attr("class", "legend")
-    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
- 
- legend.append("rect")
-    .attr("x", width - 18)
-    .attr("width", 18)
-    .attr("height", 18)
-    .style("fill", color);
- 
- legend.append("text")
-    .attr("x", width - 24)
-    .attr("y", 9)
-    .attr("dy", ".35em")
-    .style("text-anchor", "end").style("font-family", 'sans-serif')
-    .text(function(d) { return d; });*/
- 
- /* Popup window
-  var tip;
-  nodes.on("dblclick", function(d){
-    if (tip) tip.remove();
- 
-    tip  = svg.append("g")
-      .attr("transform", "translate(" + d.x  + "," + d.y + ")");
- 
-    var rect = tip.append("rect")
-      .style("fill", "white")
-      .style("stroke", "steelblue");
- 
-    tip.append("text")
-      .text("Name: " + d.id)
-      .attr("dy", "1em")
-      .attr("x", 5);
- 
-    tip.append("text")
-      .text("Continent: " + d.group)
-      .attr("dy", "2em")
-      .attr("x", 5);
- 
-    var con = graph.links
-      .filter(function(d1){
-        return d1.source.id === d.id;
-      })
-      .map(function(d1){
-        return d1.target.id + " with weight " + d1.count;
-      })
- 
-    tip.append("text")
-      .text("Connected to: " + con.join(","))
-      .attr("dy", "3em")
-      .attr("x", 5);
- 
-    var bbox = tip.node().getBBox();
-    rect.attr("width", bbox.width + 5)
-        .attr("height", bbox.height + 5)
-  });*/
- //function initialize(results) {}
- 
- /* SEARCH functionality
-    var optArray = [];
-    for (var i = 0; i < graph.nodes.length - 1; i++) {
-        optArray.push(graph.nodes[i].id);
     }
  
-    optArray = optArray.sort();
  
-    $(function () {
-        $("#search").autocomplete({
-            source: optArray
-        });
-    });
-    function searchNode() {
+//add zoom capabilities
+    var zoom_handler = d3.zoom()
+        .on("zoom", zoom_actions);
  
-    //find the node
- 
-    var selectedVal = document.getElementById('search').value;
-    var node = svg.selectAll(".node");
- 
-    if (selectedVal == "none") {
-        nodes.style("stroke", "white").style("stroke-width", "1");
-    } else {
-        var selected = node.filter(function (d, i) {
-            return d.name != selectedVal;
-        });
-        selected.style("opacity", "0");
-        var link = svg.selectAll(".link")
-        link.style("opacity", "0");
-        d3.selectAll(".node, .link").transition()
-            .duration(5000)
-            .style("opacity", 1);
- 
+    zoom_handler(svg);
+//Zoom functions
+    function zoom_actions(){
+        //if (positioning === 'map') { return }
+        g.attr("transform", d3.event.transform)
     }
-}*/
  
-   });
+    function tickActions() {
+        links
+            .attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+ 
+        nodes
+            .attr("transform", function(d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            })
+    }
+ 
+    /* LEGEND
+    var legend = svg.selectAll(".legend")
+        .data(color.domain())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+ 
+     legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
+ 
+     legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end").style("font-family", 'sans-serif')
+        .text(function(d) { return d; });*/
+ 
+    /* Popup window
+     var tip;
+     nodes.on("dblclick", function(d){
+       if (tip) tip.remove();
+ 
+       tip  = svg.append("g")
+         .attr("transform", "translate(" + d.x  + "," + d.y + ")");
+ 
+       var rect = tip.append("rect")
+         .style("fill", "white")
+         .style("stroke", "steelblue");
+ 
+       tip.append("text")
+         .text("Name: " + d.id)
+         .attr("dy", "1em")
+         .attr("x", 5);
+ 
+       tip.append("text")
+         .text("Continent: " + d.group)
+         .attr("dy", "2em")
+         .attr("x", 5);
+ 
+       var con = graph.links
+         .filter(function(d1){
+           return d1.source.id === d.id;
+         })
+         .map(function(d1){
+           return d1.target.id + " with weight " + d1.count;
+         })
+ 
+       tip.append("text")
+         .text("Connected to: " + con.join(","))
+         .attr("dy", "3em")
+         .attr("x", 5);
+ 
+       var bbox = tip.node().getBBox();
+       rect.attr("width", bbox.width + 5)
+           .attr("height", bbox.height + 5)
+     });*/
+    //function initialize(results) {}
+ 
+    /* SEARCH functionality
+       var optArray = [];
+       for (var i = 0; i < graph.nodes.length - 1; i++) {
+           optArray.push(graph.nodes[i].id);
+       }
+ 
+       optArray = optArray.sort();
+ 
+       $(function () {
+           $("#search").autocomplete({
+               source: optArray
+           });
+       });
+       function searchNode() {
+ 
+       //find the node
+ 
+       var selectedVal = document.getElementById('search').value;
+       var node = svg.selectAll(".node");
+ 
+       if (selectedVal == "none") {
+           nodes.style("stroke", "white").style("stroke-width", "1");
+       } else {
+           var selected = node.filter(function (d, i) {
+               return d.name != selectedVal;
+           });
+           selected.style("opacity", "0");
+           var link = svg.selectAll(".link")
+           link.style("opacity", "0");
+           d3.selectAll(".node, .link").transition()
+               .duration(5000)
+               .style("opacity", 1);
+ 
+       }
+   }*/
+ 
+});
  
 function dragStarted(d) {
     if (positioning === 'map') { return }
